dea71a7f2053b967ef5347c5f2903924776c290d
Yan233_ <Yan233th@126.com>
2025-05-19 01:12:12 +0800

    支持从环境变量中读取json文件path和监听地址

--- Backend/src/main.rs
+++ Backend/src/main.rs
@@ -4,22 +4,23 @@ use axum::{
     routing::get,
 };
 use serde_json::{Value, json};
-use std::{fs, net::SocketAddr};
+use std::{env, fs, net::SocketAddr};
 use tower_http::cors::CorsLayer;
 
-const TODOS_FILE_PATH: &str = "todos.json";
+// const TODOS_FILE_PATH: &str = "todos.json";
 
 async fn get_todos() -> Json<Value> {
-    match fs::read_to_string(TODOS_FILE_PATH) {
+    let todos_file_path = env::var("TODOS_FILE_PATH").unwrap_or("todos.json".to_owned());
+    match fs::read_to_string(&todos_file_path) {
         Ok(data_str) => match serde_json::from_str(&data_str) {
             Ok(json_value) => Json(json_value),
             Err(parse_error) => {
-                eprintln!("Error: Failed to parse JSON from '{}': {}. Returning empty list.", TODOS_FILE_PATH, parse_error);
+                eprintln!("Error: Failed to parse JSON from '{}': {}. Returning empty list.", &todos_file_path, parse_error);
                 Json(json!([]))
             }
         },
         Err(file_err) => {
-            eprintln!("Error: Failed to read file '{}': {}. Returning empty list.", TODOS_FILE_PATH, file_err);
+            eprintln!("Error: Failed to read file '{}': {}. Returning empty list.", &todos_file_path, file_err);
             Json(json!([]))
         }
     }
@@ -32,7 +33,8 @@ async fn main() {
         .allow_methods(vec![Method::GET])
         .allow_headers(vec![CONTENT_TYPE]);
     let app: Router<()> = Router::new().route("/todos", get(get_todos)).layer(cors_layer);
-    let addr: SocketAddr = "127.0.0.1:3000".parse().unwrap();
+    // let addr: SocketAddr = "127.0.0.1:3072".parse().unwrap();
+    let addr: SocketAddr = env::var("LISTEN_ADDRESS").unwrap_or("127.0.0.1:3072".to_owned()).parse().expect("Invalid LISTEN_ADDRESS format");
     println!("Backend server listening on http://{}", addr);
     match tokio::net::TcpListener::bind(addr).await {
         Ok(listener) => {
